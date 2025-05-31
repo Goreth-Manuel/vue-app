c399fb06ba8c57114c2c137f92db8701602ba1f3
Goreth Manuel <gorethmanuel0@gmail.com>
2025-05-31 21:48:23 +0100

    Finaliza a criação de páginas :sparkles:

---
+++ pages/lista.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>Lista</div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped></style>
--- pages/videos/[id].vue
+++ pages/videos/[id].vue
@@ -1,7 +1,11 @@
 <template>
-  <div>Você está no vídeo número {{ $route.params.id }}</div>
+  <div>Você está no vídeo número {{ idParams }}</div>
 </template>
 
-<script setup></script>
+<script setup>
+const route = useRoute();
+
+const idParams = computed(() => route.params.id);
+</script>
 
 <style lang="scss" scoped></style>
