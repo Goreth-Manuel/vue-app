ed95f987369b3e504a3983a58fe6e65c45b53d51
Goreth Manuel <gorethmanuel0@gmail.com>
2025-06-03 23:33:45 +0100

    Adiciona funções na composable :sparkles:

--- components/video/Detalhes.vue
+++ components/video/Detalhes.vue
@@ -1,4 +1,13 @@
+<script setup lang="ts">
+const {diasPassados, diferencaEmDias} = useFormatacao();
+
+const dataVideo = new Date(2023, 9, 2)
+</script>
+
+
 <template>
     <h1>Componentes - Nuxt 3</h1>
     <p>Autor: Goreth Manuel</p>
+    <p>14 dias atrás</p>
+    <p>{{  diferencaEmDias }} dias atrás</p>
 </template>
